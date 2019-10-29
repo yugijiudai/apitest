7814906dbb75407660c331a07e031ee72fbced12
lml <634131831@qq.com>
2019-10-29 22:42:35 +0800

    [refactor]重构读取excel方法

--- src/main/java/com/lml/apitest/util/MyExcelUtil.java
+++ src/main/java/com/lml/apitest/util/MyExcelUtil.java
@@ -1,21 +1,20 @@
 package com.lml.apitest.util;
 
-import cn.hutool.core.convert.Convert;
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
 import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.lml.apitest.annotations.ExcelColumn;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.MapUtils;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author yugi
@@ -49,42 +48,31 @@ public class MyExcelUtil {
      * @param <T>            对应的实体类类型
      * @return 实体类列表
      */
-    public <T> List<T> loadExcel(String fileName, Class<T> clz, Integer headerRowIndex, Integer startRowIndex, Integer endRowIndex) {
+    private <T> List<T> loadExcel(String fileName, Class<T> clz, Integer headerRowIndex, Integer startRowIndex, Integer endRowIndex) {
         ExcelReader reader = ExcelUtil.getReader(ResourceUtil.getStream(fileName));
         headerRowIndex = headerRowIndex == null ? 0 : headerRowIndex;
         startRowIndex = startRowIndex == null ? 0 : startRowIndex;
         endRowIndex = endRowIndex == null ? reader.getSheetCount() : endRowIndex;
-        Map<String, Field> fieldMap = aliasField(clz);
+        Map<String, String> mapping = fieldMapping(clz);
         List<Map<String, Object>> read = reader.read(headerRowIndex, startRowIndex, endRowIndex);
-        List<T> result = Lists.newLinkedList();
-        for (Map<String, Object> map : read) {
-            T newClz = ReflectUtil.newInstance(clz);
-            map.forEach((key, val) -> {
-                Field field = MapUtils.getObject(fieldMap, key);
-                // 获取set方法
-                Method methodByName = ReflectUtil.getMethodByName(clz, true, "set" + field.getName());
-                // 将excel的值转成field的对应类型,不然这里反射set不到
-                Object convert = Convert.convert(field.getType(), val);
-                ReflectUtil.invoke(newClz, methodByName.getName(), convert);
-            });
-            result.add(newClz);
-        }
-        return result;
+        return read.stream().map(map -> BeanUtil.mapToBean(map, clz, CopyOptions.create().setFieldMapping(mapping))).collect(Collectors.toList());
     }
-
 
     /**
-     * 如果列有别名注释,则设置成别名的注释
+     * 获取属性的映射
      *
-     * @return 最后返回一个key是列的名字, val是列的Field类map
+     * @param clz excel最终映射到的实体类
+     * @return 返回映射的map
      */
-    private Map<String, Field> aliasField(Class clz) {
+    private Map<String, String> fieldMapping(Class clz) {
+        Map<String, String> mapping = Maps.newHashMap();
         Field[] fields = ReflectUtil.getFields(clz);
-        Map<String, Field> fieldMap = Maps.newHashMap();
         for (Field field : fields) {
             ExcelColumn annotation = field.getAnnotation(ExcelColumn.class);
-            fieldMap.put(annotation != null ? annotation.alias() : field.getName(), field);
+            mapping.put(annotation != null ? annotation.alias() : field.getName(), field.getName());
         }
-        return fieldMap;
+        return mapping;
     }
+
+
 }
