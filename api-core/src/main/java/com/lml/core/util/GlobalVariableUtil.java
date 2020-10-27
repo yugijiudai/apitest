25319fc076148d77c1514c875f86cf87778ea0b2
mahiru <634131831@qq.com>
2020-10-27 16:28:23 +0800

    [feat]增加获取缓存和打印所有缓存的方法

--- api-core/src/main/java/com/lml/core/util/GlobalVariableUtil.java
+++ api-core/src/main/java/com/lml/core/util/GlobalVariableUtil.java
@@ -1,9 +1,14 @@
 package com.lml.core.util;
 
 import cn.hutool.cache.CacheUtil;
+import cn.hutool.cache.impl.CacheObj;
 import cn.hutool.cache.impl.TimedCache;
 import com.lml.core.exception.BizException;
+import lombok.Getter;
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Iterator;
 
 /**
  * @author yugi
@@ -11,9 +16,11 @@ import lombok.experimental.UtilityClass;
  * @since 2019-08-08
  */
 @UtilityClass
+@Slf4j
 public class GlobalVariableUtil {
 
 
+    @Getter
     private TimedCache<String, Object> timedCache = CacheUtil.newTimedCache(1000 * 60 * 60);
 
     /**
@@ -28,6 +35,26 @@ public class GlobalVariableUtil {
             throw new BizException("找不到:" + key + "的缓存!");
         }
         return val;
+    }
+
+    /**
+     * 获取当前全部缓存
+     *
+     * @return 所有缓存详细信息的列表
+     */
+    public Iterator<CacheObj<String, Object>> getAllCache() {
+        return timedCache.cacheObjIterator();
+    }
+
+    /**
+     * 输出全部缓存
+     */
+    public void printAllCache() {
+        Iterator<CacheObj<String, Object>> cacheObjIterator = getAllCache();
+        while (cacheObjIterator.hasNext()) {
+            CacheObj<String, Object> next = cacheObjIterator.next();
+            log.info("{}={}", next.getKey(), next.getValue());
+        }
     }
 
     /**
