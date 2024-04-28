6934891eace557beb94e87e6ac1c9f795ad36dec
lml <634131831@qq.com>
2024-04-28 18:24:36 +0800

    [feat]代理类新增代理的方法，支持cglib的拦截器

--- api-core/src/main/java/com/lml/core/util/MyProxyUtil.java
+++ api-core/src/main/java/com/lml/core/util/MyProxyUtil.java
@@ -2,9 +2,12 @@ package com.lml.core.util;
 
 import cn.hutool.aop.ProxyUtil;
 import cn.hutool.aop.aspects.Aspect;
+import cn.hutool.aop.interceptor.CglibInterceptor;
+import cn.hutool.core.util.ReflectUtil;
 import com.lml.core.aspect.TraceAspect;
 import com.lml.core.exception.InitException;
 import lombok.experimental.UtilityClass;
+import net.sf.cglib.proxy.Enhancer;
 
 /**
  * @author yugi
@@ -29,8 +32,9 @@ public class MyProxyUtil {
     /**
      * 用来初始化需要用到aop的类
      *
-     * @param clz 需要初始化的类
-     * @param <T> 这个类的类型
+     * @param clz    需要初始化的类
+     * @param aspect 切面类
+     * @param <T>    这个类的类型
      * @return 初始化的对象
      */
     public <T> T initAspect(Class<T> clz, Class<? extends Aspect> aspect) {
@@ -41,4 +45,31 @@ public class MyProxyUtil {
             throw new InitException("类初始化失败!");
         }
     }
+
+
+    /**
+     * 用来初始化需要用到aop的类
+     *
+     * @param clz           需要初始化的类
+     * @param aspect        切面类
+     * @param myInterceptor 自定义的cglib拦截器
+     * @param <T>           这个类的类型
+     * @return 初始化的对象
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T proxy(Class<T> clz, Class<? extends Aspect> aspect, Class<? extends CglibInterceptor> myInterceptor) {
+        final Enhancer enhancer = new Enhancer();
+        try {
+            Aspect aspectClz = ReflectUtil.newInstance(aspect);
+            T target = clz.getDeclaredConstructor().newInstance();
+            enhancer.setSuperclass(target.getClass());
+            CglibInterceptor cglibInterceptor = myInterceptor == null ? new CglibInterceptor(target, aspectClz) : ReflectUtil.newInstance(myInterceptor, target, aspectClz);
+            enhancer.setCallback(cglibInterceptor);
+            return (T) enhancer.create();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
