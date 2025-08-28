4fc5b7be36cb2da7ae8959e09872473bcadd50e9
lml <634131831@qq.com>
2025-08-28 15:39:33 +0800

    [fix]修复使用cglib生成代理类的报错，改用springCglib的方法

--- api-core/src/main/java/com/lml/core/util/MyProxyUtil.java
+++ api-core/src/main/java/com/lml/core/util/MyProxyUtil.java
@@ -1,11 +1,11 @@
 package com.lml.core.util;
 
 import cn.hutool.aop.aspects.Aspect;
-import cn.hutool.aop.interceptor.CglibInterceptor;
+import cn.hutool.aop.interceptor.SpringCglibInterceptor;
 import cn.hutool.core.util.ReflectUtil;
 import com.lml.core.exception.InitException;
 import lombok.experimental.UtilityClass;
-import net.sf.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.Enhancer;
 
 /**
  * @author yugi
@@ -21,19 +21,19 @@ public class MyProxyUtil {
      *
      * @param clz           需要初始化的类
      * @param aspect        切面类
-     * @param myInterceptor 自定义的cglib拦截器
+     * @param myInterceptor 自定义的SpringCglibInterceptor拦截器(cglib在高版本的jdk会报错，这里改成用spring的)
      * @param <T>           这个类的类型
      * @return 初始化的对象
      */
     @SuppressWarnings("unchecked")
-    public <T> T proxy(Class<T> clz, Class<? extends Aspect> aspect, Class<? extends CglibInterceptor> myInterceptor) {
-        final Enhancer enhancer = new Enhancer();
+    public <T> T proxy(Class<T> clz, Class<? extends Aspect> aspect, Class<? extends SpringCglibInterceptor> myInterceptor) {
         try {
+            Enhancer enhancer = new Enhancer();
             Aspect aspectClz = ReflectUtil.newInstance(aspect);
             T target = clz.getDeclaredConstructor().newInstance();
             enhancer.setSuperclass(target.getClass());
-            CglibInterceptor cglibInterceptor = myInterceptor == null ? new CglibInterceptor(target, aspectClz) : ReflectUtil.newInstance(myInterceptor, target, aspectClz);
-            enhancer.setCallback(cglibInterceptor);
+            SpringCglibInterceptor springCglibInterceptor = myInterceptor == null ? new SpringCglibInterceptor(target, aspectClz) : ReflectUtil.newInstance(myInterceptor, target, aspectClz);
+            enhancer.setCallback(springCglibInterceptor);
             return (T) enhancer.create();
         }
         catch (Exception e) {
